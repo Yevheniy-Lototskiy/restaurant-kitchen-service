ad58cc7b17febf2917b4080546be5413b42accdb
Eugene <eugene.lot77@gmail.com>
2023-03-29 15:43:52 +0300

    Add Dish to admin panel

--- kitchen_service/admin.py
+++ kitchen_service/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from kitchen_service.models import Dish
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "price", "dish_type"]
+    list_filter = ["dish_type"]
+    search_fields = ["name"]
