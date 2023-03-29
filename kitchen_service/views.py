46b00399e74f9cfc0e7f7990417e4f4b60a45d7e
Eugene <eugene.lot77@gmail.com>
2023-03-29 16:38:28 +0300

    Created Home Page (made base.html, index.html, made index in view, also filled in urls)

--- kitchen_service/views.py
+++ kitchen_service/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen_service.models import Dish, Cook, DishType
+
+
+@login_required
+def index(request):
+
+    context = {
+        "num_dishes": Dish.objects.count(),
+        "num_cooks": Cook.objects.count(),
+        "num_dish_types": DishType.objects.all()
+    }
+
+    return render(request, "kitchen/index.html", context=context)
