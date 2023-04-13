04269575651bfa8054ed2ddcfc15d3a0e82c5ab6
Eugene <eugene.lot77@gmail.com>
2023-04-13 13:08:27 +0300

    Fixed secret key

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "in5mct5gqtd66^owel()b1x772d*=gdfz6*ib0brun^qsmty^6")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
